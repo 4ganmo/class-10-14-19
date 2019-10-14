cf0b39ba53c0f63c17fd2583357bc0b055a95442
Morgan <neospeck_nagrom351@aol.com>
2019-10-14 14:22:13 -0500

    Student fix with constant rating

--- Student.java
+++ Student.java
@@ -1,4 +1,26 @@
-
-public class Student {
+//imports
+public class Student extends Person implements Comparable<Student> {
+	private String className;
+	private Integer rating;
+	
+	
+	public Student() throws NameException{
+		super();
+		rating = 5;
+		
+	}
+	
+	
+	
+	@Override
+	public int compareTo(Student o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public Integer getRating() {
+		return rating;
+		
+	}
 
 }
